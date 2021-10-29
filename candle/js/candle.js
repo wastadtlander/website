6a4f04ba97c71892bca68207c13be0574b52d320
wastadtlander <66275962+wastadtlander@users.noreply.github.com>
2021-10-28 22:20:02 -0400

    24

--- candle/js/candle.js
+++ candle/js/candle.js
@@ -1,13 +1,21 @@
 let player = {
+	candles: {
+
+	},
+	fuel: {
+		have: new Decimal(50),
+		gain: new Decimal(-0.1),
+		max: new Decimal(50)
+	},
 	wax: {
 		have: new Decimal(0),
 		gain: new Decimal(0.1),
 		max: new Decimal(50)
-	}	
+	}
 }
 
 function onLoad() {
-	for (let i = 0; i <= 4; ++i) updateHTML(i);
+	for (let i = 0; i <= 8; ++i) updateHTML(i);
 }
 
 function updateHTML(element) {
@@ -19,8 +27,8 @@ function updateHTML(element) {
 			break;
 		/* Wax bar */
 		case 2:
-			let percent = new Decimal((player.wax.have.div(player.wax.max)).mul(100));
-			document.getElementById("waxBar").style.width = percent + '%';
+			let waxPercent = new Decimal((player.wax.have.div(player.wax.max)).mul(100));
+			document.getElementById("waxBar").style.width = waxPercent + '%';
 			break;
 		/* Wax max value */
 		// case 3:
@@ -31,6 +39,25 @@ function updateHTML(element) {
 			/* TODO: Clarify how the gain is displayed depending on substraction */
 			if (player.wax.gain.lt(10000)) document.getElementById("waxGain").innerHTML = " (+" + player.wax.gain.toFixed(1) + "/s)";
 			else document.getElementById("waxGain").innerHTML = " (+" + player.wax.gain.toExponential(2) + "/s)";
+		/* Fuel have value */					
+		case 5:
+			if (player.wax.have.lt(10000)) document.getElementById("fuelHave").innerHTML = player.fuel.have.toFixed(0);
+			else document.getElementById("fuelHave").innerHTML = player.fuel.have.toExponential(2);
+			break;
+		/* Fuel bar */
+		case 6:
+			let fuelPercent = new Decimal((player.fuel.have.div(player.fuel.max)).mul(100));
+			document.getElementById("fuelBar").style.width = fuelPercent + '%';
+			break;
+		/* Fuel max value */
+		// case 7:
+		// 	document.getElementById("fuelMax").innerHTML = player.fuel.max;
+		// 	break;
+		/* Fuel gain value */
+		case 8:
+			/* TODO: Clarify how the gain is displayed depending on substraction */
+			if (player.fuel.gain.lt(10000)) document.getElementById("fuelGain").innerHTML = " (" + player.fuel.gain.toFixed(1) + "/s)";
+			else document.getElementById("fuelGain").innerHTML = " (" + player.fuel.gain.toExponential(2) + "/s)";
 		default:
 			break;
 	}
@@ -39,13 +66,15 @@ function updateHTML(element) {
 /* Main game loop */
 onLoad();
 let game = setInterval(() => {
-	if (player.wax.have.lt(player.wax.max)) {
-		player.wax.have = player.wax.have.add(player.wax.gain);
-		updateHTML(1);
-		updateHTML(2);
-	} else if (player.wax.have.gt(player.wax.max)) {
-		player.wax.have = player.wax.max;
-		updateHTML(1);
-		updateHTML(2);
-	}
+	if (player.wax.have.lt(player.wax.max)) player.wax.have = player.wax.have.add(player.wax.gain);
+	else if (player.wax.have.gt(player.wax.max)) player.wax.have = player.wax.max;
+
+	updateHTML(1);
+	updateHTML(2);
+
+	if (player.fuel.have.gt(0)) player.fuel.have = player.fuel.have.add(player.fuel.gain);
+	else if (player.fuel.have.lt(0)) player.fuel.have = 0;
+
+	updateHTML(5);
+	updateHTML(6);
 }, 50);
