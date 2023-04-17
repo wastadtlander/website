ed0851552c461583bab0f5858afdb800b3a1e155
William Stadtlander <wastadtlander@vt.edu>
2023-04-17 12:06:24 -0400

    game

--- game/game.js
+++ game/game.js
@@ -1,20 +1,41 @@
 let player = {
     game: {
         points: {
-            amount: 0,
-            change: 0.1,
+            amount: new Decimal(0),
+            change: new Decimal(0.1),
         },
         speed: {
-            min: 1,
-            max: 1.5, 
-            step: 0.01,
-            value: 1
+            min: new Decimal(0.99),
+            max: new Decimal(1e5), 
+            step: new Decimal(0.01),
+            value: new Decimal(1)
         },
     },
     settings: {
-        fps: 144
+        fps: new Decimal(30)
     }
 };
+
+let points = document.getElementById('points');
+let change = document.getElementById('change')
+let current = document.getElementById('current');
+let real = document.getElementById('real');
+
+function loop() {
+    player.game.points.amount = new Decimal(player.game.points.amount.add((player.game.points.change.div(player.settings.fps)).mul(player.game.speed.value)));
+    if (player.game.speed.value.eq(0.99)) player.game.speed.value = new Decimal(0);
+    updateVisuals();
+}
+
+function updateVisuals() {
+    if (player.game.points.amount.gte(new Decimal(1e3))) points.innerHTML = player.game.points.amount.toExponential(2);
+    else points.innerHTML = player.game.points.amount.toFixed(2);
+    if (player.game.points.change.gte(new Decimal(1e3))) change.innerHTML = player.game.points.change.toExponential(2);
+    else points.innerHTML = player.game.points.change.toFixed(2);
+    if (player.game.points.current.gte(new Decimal(1e3))) current.innerHTML = player.game.points.current.toExponential(2);
+    else points.innerHTML = player.game.points.current.toFixed(2);
+    real.innerHTML = (player.game.points.change.mul(player.game.speed.value)).toFixed(2);
+}
 
 window.onload = () => {
     let speed = document.getElementById('speed');
@@ -23,27 +44,7 @@ window.onload = () => {
     speed.step = player.game.speed.step;
     speed.value = player.game.speed.value;
     
-    speed.addEventListener('input', () => {
-      player.game.speed.value = parseFloat(speed.value);
-    });
+    speed.addEventListener('input', () => { player.game.speed.value = new Decimal(parseFloat(speed.value)); });
 }
 
-let points = document.getElementById('points');
-let change = document.getElementById('change')
-let current = document.getElementById('current');
-let real = document.getElementById('real');
-
-function loop() {
-    player.game.points.amount += (player.game.points.change / player.settings.fps) * player.game.speed.value;
-    updateVisuals();
-}
-
-function updateVisuals() {
-    points.innerHTML = player.game.points.amount.toFixed(2);
-    change.innerHTML = player.game.points.change.toFixed(2);
-    current.innerHTML = player.game.speed.value.toFixed(2);
-    real.innerHTML = (player.game.points.change *  player.game.speed.value).toFixed(2);
-}
-
-
-setInterval(loop, 1000 / player.settings.fps)
+setInterval(loop, new Decimal(1000).div(player.settings.fps))
